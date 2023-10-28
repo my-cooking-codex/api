9c54b93c8e29da522f81256a9d8bfa86beeb70f0
Leo Spratt <enchant97@users.noreply.github.com>
2023-10-28 12:11:24 +0100

    include all used labels by user in query

--- db/crud/labels.go
+++ db/crud/labels.go
@@ -3,29 +3,61 @@ package crud
 import (
 	"github.com/google/uuid"
 	"github.com/my-cooking-codex/api/db"
+	"gorm.io/gorm"
 )
 
 func GetLabelNamesByUser(userID uuid.UUID) ([]string, error) {
 	var labels []string
-	if err := db.DB.Model(&db.Recipe{}).
-		Distinct("labels.name").
-		Joins("JOIN recipe_labels ON recipes.id = recipe_labels.recipe_id").
-		Joins("JOIN labels ON recipe_labels.label_id = labels.id").
-		Where("recipes.owner_id = ?", userID).
-		Pluck("name", &labels).Error; err != nil {
-		return nil, err
-	}
-	return labels, nil
+	tx := db.DB.Session(&gorm.Session{PrepareStmt: true})
+	err := tx.
+		Model(&db.User{}).
+		Raw(`SELECT DISTINCT d1.name AS label_name
+FROM (
+    SELECT labels.name
+    FROM users u
+    LEFT JOIN recipes r ON u.id = r.owner_id
+    LEFT JOIN recipe_labels r1 ON r.id = r1.recipe_id
+    LEFT JOIN labels ON r1.label_id = labels.id
+    WHERE u.id = ? AND labels.name IS NOT NULL
+
+    UNION
+
+    SELECT labels.name
+    FROM users u
+    LEFT JOIN pantry_locations pl ON u.id = pl.owner_id
+    LEFT JOIN pantry_items pi ON pl.id = pi.location_id
+    LEFT JOIN pantry_item_labels pil ON pi.id = pil.pantry_item_id
+    LEFT JOIN labels ON pil.label_id = labels.id
+    WHERE u.id = ? AND labels.name IS NOT NULL
+) AS d1;`, userID, userID).
+		Scan(&labels).Error
+	return labels, err
 }
 
 func GetLabelCountByUser(userID uuid.UUID) (int64, error) {
 	var count int64
-	if err := db.DB.Model(&db.Recipe{}).
-		Distinct("recipe_labels.label_id").
-		Joins("JOIN recipe_labels ON recipes.id = recipe_labels.recipe_id").
-		Where("recipes.owner_id = ?", userID).
-		Count(&count).Error; err != nil {
-		return 0, err
-	}
-	return count, nil
+	tx := db.DB.Session(&gorm.Session{PrepareStmt: true})
+	err := tx.
+		Model(&db.User{}).
+		Raw(`SELECT COUNT(DISTINCT d1.id) AS label_count
+FROM (
+    SELECT l.id
+    FROM users u
+    LEFT JOIN recipes r ON u.id = r.owner_id
+    LEFT JOIN recipe_labels r1 ON r.id = r1.recipe_id
+    LEFT JOIN labels l ON r1.label_id = l.id
+	WHERE u.id = ?
+
+    UNION
+
+    SELECT l.id
+    FROM users u
+    LEFT JOIN pantry_locations pl ON u.id = pl.owner_id
+    LEFT JOIN pantry_items pi ON pl.id = pi.location_id
+    LEFT JOIN pantry_item_labels pil ON pi.id = pil.pantry_item_id
+    LEFT JOIN labels l ON pil.label_id = l.id
+	WHERE u.id = ?
+) AS d1;`, userID, userID).
+		Scan(&count).Error
+	return count, err
 }
